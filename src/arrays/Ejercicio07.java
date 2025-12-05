f01de35e6b93d636d40b110c2e77f270c95949f8
aajjfloridoo <aj.florido@iesnervion.es>
2025-12-05 12:30:12 +0100

    wdf

--- src/arrays/Ejercicio07.java
+++ src/arrays/Ejercicio07.java
@@ -1,10 +1,47 @@
 package arrays;
 
+import java.util.Scanner;
+
 public class Ejercicio07 {
 
 	public static void main(String[] args) {
 
-		
+        Scanner sc = new Scanner(System.in);
+
+        String[] meses = {
+            "Enero", "Febrero", "Marzo", "Abril",
+            "Mayo", "Junio", "Julio", "Agosto",
+            "Septiembre", "Octubre", "Noviembre", "Diciembre"
+        };
+
+        double[] temperaturas = new double[12];
+
+        for (int i = 0; i < 12; i++) {
+            System.out.print("Introduce la temperatura media de " + meses[i] + ": ");
+            temperaturas[i] = sc.nextDouble();
+        }
+
+        System.out.println("\n--------DIAGRAMA DE BARRAS DE TEMPERATURAS--------");
+
+        for (int i = 0; i < 12; i++) {
+            System.out.printf("%-10s | ", meses[i]);
+
+            int barras = 0;
+            double temp = temperaturas[i];
+
+            while (temp >= 1) {
+                barras++;
+                temp = temp - 1;
+            }
+
+            for (int j = 0; j < barras; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println(" (" + temperaturas[i] + " ºC)");
+        }
+
+        sc.close();	
 		
 		
 		
